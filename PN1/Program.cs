92695c75a8086a1b1e4296e307ec8b21e3344389
Demian George <georgedemian28@gmail.com>
2020-11-24 09:38:03 +0200

    Prima problema de la numere

---
+++ PN1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PN1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            
+            Console.Write("a = ");
+            string an = Console.ReadLine();
+            Console.Write("b = ");
+            string bn = Console.ReadLine();
+
+            a = int.Parse(an);
+            b = int.Parse(bn);
+            b = b * -1;
+            double x = (double)b / a;
+
+            Console.WriteLine("ax = b = 0");
+            Console.WriteLine($"=> {a}x + {-b} = 0");
+            Console.WriteLine($"=> {a}x = {b}");
+            Console.WriteLine($"=> x = {b} / {a}");
+            Console.WriteLine($"=> x = {x}");
+        }
+    }
+}
